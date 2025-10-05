ae0bd62aa78648c897f2f93205b225611a052d87
BergJulian <julianberg.de@gmail.com>
2025-10-05 22:43:07 +0200

    build fix

--- ns-3/build.sh
+++ ns-3/build.sh
@@ -46,7 +46,6 @@ section ---------------- ns-3 ----------------
 workdir "/opt/ns-3/ns-$NS3_VERSION"
 run mkdir -p build
 workdir "/opt/ns-3/ns-$NS3_VERSION/build"
-run make clean
 run cmake ..
 make -j$(nproc) 2>&1 | tee build.log
 
